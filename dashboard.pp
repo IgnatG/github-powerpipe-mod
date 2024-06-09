4a6aeb458c8dd5d41c04f3ac61976e3664dec858
Gabriel Ignat <gabriel.ignat@hotmail.com>
2024-06-09 19:20:31 +0100

    first commit
    feat: added github stats
    
    feat: updated the configs
    
    feat: debug dashboard
    
    fix: updated the stats table
    
    fix: removed the plugin attribute
    
    fix: updated the stats table
    
    fix: updated the stats table
    
    fix: updated the stats table
    
    fix: updated the stats table
    
    fix: updated the stats table
    
    fix: updated the stats table
    
    fix: updated the stats table
    
    fix: updated the stats table
    
    fix: updated the stats table
    
    fix: updated the stats table
    
    feat: added more charts
    
    feat: added more charts

---
+++ dashboard.pp
@@ -0,0 +1,159 @@
+dashboard "github_branch_counts_dashboard" {  
+  container {
+    table {
+      title = "Table - Branch Counts by Repository"
+      width = 6
+      sql = <<EOQ
+        WITH repositories AS (
+          SELECT
+            REPLACE(url, 'https://github.com/', '') AS repository_full_name,
+            url
+          FROM
+            github_my_repository
+        ),
+        branch_counts AS (
+          SELECT
+            repository_full_name,
+            COUNT(name) AS branch_count
+          FROM
+            github_branch
+          WHERE
+            repository_full_name IN (
+              SELECT
+                repository_full_name
+              FROM
+                repositories
+            )
+          GROUP BY
+            repository_full_name
+        )
+        SELECT
+          r.repository_full_name AS "Repository name",
+          COALESCE(b.branch_count, 0) AS "Total branches"
+        FROM
+          repositories r
+          LEFT JOIN branch_counts b
+          ON r.repository_full_name = b.repository_full_name
+        ORDER BY
+          "Total branches" DESC;
+      EOQ
+    }
+
+    chart {
+      title = "Bar Chart - Branch Counts by Repository"
+      type = "bar"
+      width = 6
+      sql = <<EOQ
+        WITH repositories AS (
+          SELECT
+            REPLACE(url, 'https://github.com/', '') AS repository_full_name
+          FROM
+            github_my_repository
+        ),
+        branch_counts AS (
+          SELECT
+            repository_full_name,
+            COUNT(name) AS branch_count
+          FROM
+            github_branch
+          WHERE
+            repository_full_name IN (
+              SELECT
+                repository_full_name
+              FROM
+                repositories
+            )
+          GROUP BY
+            repository_full_name
+        )
+        SELECT
+          r.repository_full_name,
+          COALESCE(b.branch_count, 0) AS branch_count
+        FROM
+          repositories r
+          LEFT JOIN branch_counts b
+          ON r.repository_full_name = b.repository_full_name
+        ORDER BY
+          branch_count DESC;
+      EOQ
+    }
+
+    chart {
+      title = "Column Chart - Branch Counts by Repository"
+      type = "column"
+      width = 12
+      sql = <<EOQ
+        WITH repositories AS (
+          SELECT
+            REPLACE(url, 'https://github.com/', '') AS repository_full_name
+          FROM
+            github_my_repository
+        ),
+        branch_counts AS (
+          SELECT
+            repository_full_name,
+            COUNT(name) AS branch_count
+          FROM
+            github_branch
+          WHERE
+            repository_full_name IN (
+              SELECT
+                repository_full_name
+              FROM
+                repositories
+            )
+          GROUP BY
+            repository_full_name
+        )
+        SELECT
+          r.repository_full_name,
+          COALESCE(b.branch_count, 0) AS branch_count
+        FROM
+          repositories r
+          LEFT JOIN branch_counts b
+          ON r.repository_full_name = b.repository_full_name
+        ORDER BY
+          branch_count DESC;
+      EOQ
+    }
+
+    chart {
+      title = "Pie Chart - Branch Counts by Repository"
+      type = "pie"
+      width = 12
+      sql = <<EOQ
+        WITH repositories AS (
+          SELECT
+            REPLACE(url, 'https://github.com/', '') AS repository_full_name
+          FROM
+            github_my_repository
+        ),
+        branch_counts AS (
+          SELECT
+            repository_full_name,
+            COUNT(name) AS branch_count
+          FROM
+            github_branch
+          WHERE
+            repository_full_name IN (
+              SELECT
+                repository_full_name
+              FROM
+                repositories
+            )
+          GROUP BY
+            repository_full_name
+        )
+        SELECT
+          r.repository_full_name,
+          COALESCE(b.branch_count, 0) AS branch_count
+        FROM
+          repositories r
+          LEFT JOIN branch_counts b
+          ON r.repository_full_name = b.repository_full_name
+        ORDER BY
+          branch_count DESC;
+      EOQ
+    }
+  }
+}
---
+++ mod.pp
@@ -0,0 +1,5 @@
+mod "github" {
+  title = "Branches in GitHub repositories"
+  name = "GitHub Branch Count Mod"
+  description = "A mod to count branches in GitHub repositories"
+}
