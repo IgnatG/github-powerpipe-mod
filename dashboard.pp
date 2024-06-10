02f9613c84374ed9ff7b9ed0ff5578242b31859d
Gabriel Ignat <gabriel.ignat@hotmail.com>
2024-06-10 09:06:30 +0100

    fix: fixed the empty description

--- dashboard.pp
+++ dashboard.pp
@@ -62,54 +62,54 @@ dashboard "github_branch_counts_dashboard" {
           github_my_repository
         WHERE
           url LIKE 'https://github.com/UKHSA-Internal/edap%'
-          AND (description IS NULL OR description = 'This is a description of the repo')
+          AND (description IS NULL OR description = 'This is a description of the repo' OR description = '')
       EOQ
       width = 3
     }
   }
 
-      chart {
-      title = "Column Chart - Branch Counts by Repository"
-      type = "column"
-      width = 12
-      sql = <<EOQ
-        WITH repositories AS (
-          SELECT
-            url,
-            REPLACE(url, 'https://github.com/', '') AS repository_full_name
-          FROM
-            github_my_repository
-          WHERE
-            url LIKE 'https://github.com/UKHSA-Internal/edap%'
-        ),
-        branch_counts AS (
-          SELECT
-            repository_full_name,
-            COUNT(name) AS branch_count
-          FROM
-            github_branch
-          WHERE
-            repository_full_name IN (
-              SELECT
-                repository_full_name
-              FROM
-                repositories
-            )
-          GROUP BY
-            repository_full_name
-        )
+    chart {
+    title = "Column Chart - Branch Counts by Repository"
+    type = "column"
+    width = 12
+    sql = <<EOQ
+      WITH repositories AS (
         SELECT
-          r.url AS "Repository URL",
-          COALESCE(b.branch_count, 0) AS branch_count
+          url,
+          REPLACE(url, 'https://github.com/', '') AS repository_full_name
         FROM
-          repositories r
-          LEFT JOIN branch_counts b
-          ON r.repository_full_name = b.repository_full_name
-        ORDER BY
-          branch_count DESC
-        LIMIT 20;
-      EOQ
-    }
+          github_my_repository
+        WHERE
+          url LIKE 'https://github.com/UKHSA-Internal/edap%'
+      ),
+      branch_counts AS (
+        SELECT
+          repository_full_name,
+          COUNT(name) AS branch_count
+        FROM
+          github_branch
+        WHERE
+          repository_full_name IN (
+            SELECT
+              repository_full_name
+            FROM
+              repositories
+          )
+        GROUP BY
+          repository_full_name
+      )
+      SELECT
+        r.url AS "Repository URL",
+        COALESCE(b.branch_count, 0) AS branch_count
+      FROM
+        repositories r
+        LEFT JOIN branch_counts b
+        ON r.repository_full_name = b.repository_full_name
+      ORDER BY
+        branch_count DESC
+      LIMIT 20;
+    EOQ
+  }
 
   container {
     table {
