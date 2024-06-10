b6eb334ada16f0accb56999fc16464e842030710
Gabriel Ignat <gabriel.ignat@hotmail.com>
2024-06-10 22:04:38 +0100

    fix: removed column chart

--- dashboard.pp
+++ dashboard.pp
@@ -83,50 +83,6 @@ dashboard "github_branch_counts_dashboard" {
       type = "info"
     }
   }
-  container {
-    chart {
-      title = "Top 20 Branch Counts by Repository"
-      type = "column"
-      width = 12
-      sql = <<EOQ
-        WITH repositories AS (
-          SELECT
-            url,
-            REPLACE(url, 'https://github.com/', '') AS repository_full_name
-          FROM
-            github_my_repository
-          WHERE
-            url LIKE 'https://github.com/UKHSA-Internal/edap%'
-        ),
-        branch_counts AS (
-          SELECT
-            repository_full_name,
-            COUNT(name) AS branch_count
-          FROM
-            github_branch
-          WHERE
-            repository_full_name IN (
-              SELECT
-                repository_full_name
-              FROM
-                repositories
-            )
-          GROUP BY
-            repository_full_name
-        )
-        SELECT
-          r.repository_full_name,
-          COALESCE(b.branch_count, 0) AS branch_count
-        FROM
-          repositories r
-          LEFT JOIN branch_counts b
-          ON r.repository_full_name = b.repository_full_name
-        ORDER BY
-          branch_count DESC
-        LIMIT 20;
-      EOQ
-    }
-  }
 
   container {
     table {
@@ -136,8 +92,6 @@ dashboard "github_branch_counts_dashboard" {
           SELECT
             REPLACE(url, 'https://github.com/', '') AS repository_full_name,
             url,
-            description,
-            updated_at,
             pushed_at,
             is_archived
           FROM
@@ -162,7 +116,6 @@ dashboard "github_branch_counts_dashboard" {
             repository_full_name
         )
         SELECT
-          r.repository_full_name AS "Repository Name",
           r.url AS "Repository URL",
           r.pushed_at AS "Last Push",
           r.is_archived AS "Is Archived",
