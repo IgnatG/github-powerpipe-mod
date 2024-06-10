ae907ffdb11d37266d2ebfd2cbfb8581db531c5e
Gabriel Ignat <gabriel.ignat@hotmail.com>
2024-06-10 08:08:54 +0100

    fix: removed the pie chart

--- dashboard.pp
+++ dashboard.pp
@@ -116,44 +116,5 @@ dashboard "github_branch_counts_dashboard" {
           branch_count DESC;
       EOQ
     }
-
-    chart {
-      title = "Pie Chart - Branch Counts by Repository"
-      type = "pie"
-      width = 12
-      sql = <<EOQ
-        WITH repositories AS (
-          SELECT
-            REPLACE(url, 'https://github.com/', '') AS repository_full_name
-          FROM
-            github_my_repository
-        ),
-        branch_counts AS (
-          SELECT
-            repository_full_name,
-            COUNT(name) AS branch_count
-          FROM
-            github_branch
-          WHERE
-            repository_full_name IN (
-              SELECT
-                repository_full_name
-              FROM
-                repositories
-            )
-          GROUP BY
-            repository_full_name
-        )
-        SELECT
-          r.repository_full_name,
-          COALESCE(b.branch_count, 0) AS branch_count
-        FROM
-          repositories r
-          LEFT JOIN branch_counts b
-          ON r.repository_full_name = b.repository_full_name
-        ORDER BY
-          branch_count DESC;
-      EOQ
-    }
   }
 }
